cf4b454d57b605a485d9ba9fe08f8c167f8c3fa0
forgottenmnemonic <jbobackjr@gmail.com>
2022-12-19 23:39:29 -0800

    changed some things, added custom kitty configs

--- config.sh
+++ config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mv /Wallpapers ~/john/Pictures/Wallpapers
+mv Wallpapers ~/john/Pictures/
 
-mv /config/kitty/kitty.conf ~/.config/kitty
+mv config/kitty/kitty.conf ~/.config/kitty
 
-mv /font/*.ttf /usr/share/fonts/TTF
+mv config/kitty/themes ~/.config/kitty
 
-mv /config/.bashrc ~/.bashrc
+mv font/*.ttf /usr/share/fonts/TTF
+
+mv config/.bashrc ~/.bashrc
